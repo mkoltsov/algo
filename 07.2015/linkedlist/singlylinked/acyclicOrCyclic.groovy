56ab3d0cdec1a2aa0eb7f5806b3161c19680c7b5
mkoltsov <root@javabean.ru>
2015-04-09 23:31:51 +0200

    cyclic list added

--- 07.2015/linkedlist/singlylinked/acyclicOrCyclic.groovy
+++ 07.2015/linkedlist/singlylinked/acyclicOrCyclic.groovy
@@ -2,8 +2,7 @@ class SinglyLinkedList<T> {
 	ListElement<T> head
 	ListElement<T> tail
 
-	def enque(T value) {
-		def newNode = new ListElement<T>(value:value)
+	def enque(ListElement<T> newNode) {
 		if (!head) {
 			head = newNode
 			tail = newNode
@@ -11,48 +10,54 @@ class SinglyLinkedList<T> {
 				tail.next = newNode
 				tail = newNode
 			}
+	}
+
+	def iterateWithClosure(def clos) {
+		def newHead = head
+
+		while(newHead) {
+			clos(newHead)
+			newHead = newHead.next
 		}
+	} 
 
-		def iterateWithClosure(def clos) {
-			def newHead = head
+	def acyclicOrCyclic(){
+		def clos = {_ -> 
+			if (_.next == null) {
+				println("acyclic")
+				}}
+				iterateWithClosure(clos)
+	}
 
-			while(newHead) {
-				clos(newHead)
-				newHead = newHead.next
-			}
-		} 
+	def deque (){
+		head = head.next
+	}
 
-		def acyclicOrCyclic(){
-			def clos = {_ -> 
-				if (_.next == null) {
-						println("acyclic")
-					}}
-			iterateWithClosure(clos)
-		}
+	def print() {
+		def pr = {_ -> println(_.value)}
+		iterateWithClosure(pr)
+	}
+}
 
-		def deque (){
-			head = head.next
-		}
-
-		def print() {
-			def pr = {_ -> println(_.value)}
-			iterateWithClosure(pr)
-		}
-
-		class ListElement<T> {
-			ListElement<T> next
-			T value
-		}
+	class ListElement<T> {
+		ListElement<T> next
+		T value
 	}
 
 	def list = new SinglyLinkedList<String>()
 
-	list.enque("Chef")
-	list.enque("Papa")
-	list.enque("Pupa")
-	list.enque("Bupa")
-	list.enque("Diop")
-	list.enque("Shmiot")
+	def chef = new ListElement<String>(value:"Chef")
+	def chef1 = new ListElement<String>(value:"Papa")
+	def chef2 = new ListElement<String>(value:"Pupa")
+	def chef3 = new ListElement<String>(value:"Bupa")
+	def chef4 = new ListElement<String>(value:"Diop")
+	def chef5 = new ListElement<String>(value:"Shmiot", next:chef3)
+	list.enque(chef)
+	list.enque(chef1)
+	list.enque(chef2)
+	list.enque(chef3)
+	list.enque(chef4)
+	list.enque(chef5)
 	list.print()
 
 	list.deque()
