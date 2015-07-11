691c5fb67d0ec86b33c2f5f0fb2b51fd15291726
mkoltsov <root@javabean.ru>
2015-07-11 15:08:53 +0200

    singly linked list updating head added

--- 07.2015/linkedlist/singlelinked.groovy
+++ 07.2015/linkedlist/singlelinked.groovy
@@ -3,9 +3,22 @@ import groovy.transform.Canonical
 class ListElement<T> {
 	private T value
 	private ListElement<T> next
+	def ListElement<T> insertInFront(ListElement<T> list, T data) {
+		def l = new ListElement<T>(value:T)
+		l.next = list
+		return l
+	}
 }
+//First approach
 def ListEnd = new ListElement<String>(value:"Last")
 def ListMiddle = new ListElement<String>(value:"Last", next:ListEnd)
 def listStart = new ListElement<String>(value:"First", next:ListMiddle)
 
 println(listStart.next.next.value)
+
+//Tracking head approach
+def start = new ListElement<String>(value:"First")
+def head = start.insertInFront(new ListElement<String>(),"Second")
+head.insertInFront(new ListElement<String>(),"Third")
+
+println(head.value)
