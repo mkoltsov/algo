a85df6ac3469bbdd260a941d5eb2af7446ecdc62
mkoltsov <root@javabean.ru>
2015-07-12 15:20:25 +0200

    finding an element in singly linked list example added

--- 07.2015/linkedlist/singlelinked.groovy
+++ 07.2015/linkedlist/singlelinked.groovy
@@ -8,11 +8,33 @@ class ListElement<T> {
 		l.next = list
 		return l
 	}
+
+	def static findElement(ListElement<T> head, T data) {
+		def elem = head
+		while(elem && elem.value!=data) {
+			elem = elem.next
+		}
+
+		return elem
+	}
 }
 //First approach
 def ListEnd = new ListElement<String>(value:"Last")
-def ListMiddle = new ListElement<String>(value:"Last", next:ListEnd)
+def ListMiddle10 = new ListElement<String>(value:"ListEnd", next:ListEnd)
+def ListMiddle9 = new ListElement<String>(value:"ListMiddle10", next:ListMiddle10)
+def ListMiddle8 = new ListElement<String>(value:"ListMiddle9", next:ListMiddle9)
+def ListMiddle7 = new ListElement<String>(value:"ListMiddle8", next:ListMiddle8)
+def ListMiddle6 = new ListElement<String>(value:"ListMiddle7", next:ListMiddle7)
+def ListMiddle5 = new ListElement<String>(value:"ListMiddle6", next:ListMiddle6)
+def ListMiddle4 = new ListElement<String>(value:"ListMiddle5", next:ListMiddle5)
+def ListMiddle3 = new ListElement<String>(value:"ListMiddle4", next:ListMiddle4)
+def ListMiddle2 = new ListElement<String>(value:"ListMiddle3", next:ListMiddle3)
+def ListMiddle1 = new ListElement<String>(value:"ListMiddle2", next:ListMiddle2)
+def ListMiddle = new ListElement<String>(value:"ListMiddle1", next:ListMiddle1)
 def listStart = new ListElement<String>(value:"First", next:ListMiddle)
+
+println(listStart.findElement(listStart, "ListEnd").value)
+println(listStart.findElement(listStart, "Chef"))
 
 println(listStart.next.next.value)
 
