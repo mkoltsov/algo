32403248d109073eb322db105e648299a3e02ded
mkoltsov <root@javabean.ru>
2015-07-20 20:15:57 +0200

    list implemented

--- 07.2015/linkedlist/tailpointer.groovy
+++ 07.2015/linkedlist/tailpointer.groovy
@@ -5,24 +5,41 @@ class LinkedListList<T> {
 	def LinkedListElement<T> tail
 
 	def addNew(T value){
-		
-		tail = new LinkedListElement<T>(value: value, next: null)
+		def newNode  = new LinkedListElement<T>(value: value)
+		if (!head) {
+			head = newNode
+			tail = newNode
+			} else {
+				tail.next = newNode
+			}
+			tail = newNode
+			tail.next = null
+		}
+
+		def deleteHead(LinkedListElement deleted){
+
+		}
+
+		private class LinkedListElement<T> {
+			def T value
+			def LinkedListElement<T> next
+		}
 	}
 
-	def deleteHead(LinkedListElement deleted){
-		
+
+	def list = new LinkedListList<String>()
+	list.addNew("Chef")
+	list.addNew("Chef2")
+	list.addNew("Chef3")
+	list.addNew("Chef4")
+	list.addNew("Che5")
+	list.addNew("Chef7")
+	list.addNew("Chef9")
+	list.addNew("Chef11")
+	list.addNew("Chef12")
+
+	def head = list.head
+	while(head.next) {
+		println(head.value)
+		head = head.next
 	}
-
-	private class LinkedListElement<T> {
-		def T value
-		def LinkedListElement<T> next
-	}
-}
-
-
-def head = elem1
-
-while(head) {
-	println(head.value)
-	head = head.next
-}
