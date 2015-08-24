643f792b0e27276455aa792cadf30fbd92594eb0
mkoltsov <root@javabean.ru>
2015-08-24 23:59:52 +0200

    doubly linked list

--- 07.2015/linkedlist/doublylinked/listUnflattening.groovy
+++ 07.2015/linkedlist/doublylinked/listUnflattening.groovy
@@ -6,43 +6,43 @@ class DoublyLinkedList<T> {
 		if (!head) {
 			head = newNode
 			tail = newNode
-		} else {
-			tail.next = newNode
-			def previous = tail
-			tail = newNode
-			tail.previous = previous
+			} else {
+				tail.next = newNode
+				def previous = tail
+				tail = newNode
+				tail.previous = previous
+			}
+
 		}
 
+		private class LinkedListElement<T> {
+			T value
+			LinkedListElement<T> next
+			LinkedListElement<T> previous
+		}
 	}
 
-	private class LinkedListElement<T> {
-		T value
-		LinkedListElement<T> next
-		LinkedListElement<T> previous
+	def list = new DoublyLinkedList<String>()
+
+	list.enque("Chef")
+	list.enque("Pupa")
+	list.enque("Bupa")
+	list.enque("Diop")
+	list.enque("Diode")
+	list.enque("Drew")
+	list.enque("Stew")
+	list.enque("Phew")
+
+	def head = list.head
+
+	while (head) {
+		println(head.value)
+		head = head.next
 	}
-}
 
-def list = new DoublyLinkedList<String>()
+	def tail = list.tail
 
-list.enque("Chef")
-list.enque("Pupa")
-list.enque("Bupa")
-list.enque("Diop")
-list.enque("Diode")
-list.enque("Drew")
-list.enque("Stew")
-list.enque("Phew")
-
-def head = list.head
-
-while (head) {
-	println(head.value)
-	head = head.next
-}
-
-def tail = list.tail
-
-while (tail) {
-	println(tail.value)
-	tail = tail.previous
-}
+	while (tail) {
+		println(tail.value)
+		tail = tail.previous
+	}
